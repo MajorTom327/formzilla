53c02f6589e57b0d46b7c0afcbe105f44aad0d75
MajorTom327 <me@valentin-thomas.com>
2022-07-03 18:51:03 +0200

    Fix some empty build

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { FormHandler } from "./FormHandler";
+export { InputProps } from "./types/InputProps";
+export { FormField, ValidationRule } from "./FormField";
+
+// export { FormHandler as default } from "./FormHandler";
