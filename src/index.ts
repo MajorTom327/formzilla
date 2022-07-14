a3ba88c4c62d0af66552e4b3931d9bdf9243b57f
MajorTom327 <me@valentin-thomas.com>
2022-07-14 18:37:44 +0200

    Export form fields missing

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export { FormHandler } from './FormHandler';
 export { InputProps } from './types/InputProps';
-export { FormField, FormField, ValidationRule } from './FormField';
+export { FormField, FormFieldProps, ValidationRule } from './FormField';
